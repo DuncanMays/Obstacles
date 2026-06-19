64379570389e7f3c9096db2459473a2f3c842c3e
Duncan Mays <duncanmays@outlook.com>
2026-06-18 23:55:01 -0400

    adding C# version

---
+++ C#/make_course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ObstacleCourse
+{
+    public static class MakeCourse
+    {
+        public static void intro_obstacles()
+        {
+            new Obstacle(
+                    500,
+                    Globals.top_border + (Globals.bottom_border - Globals.top_border) / 2,
+                    100,
+                    Color.Cyan
+                );
+        }
+    }
+}
